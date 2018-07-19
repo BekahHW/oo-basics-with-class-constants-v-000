2efedac6ae61bf08ddaf034e441e6a1c4a96e77a
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-19 13:44:29 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
-# class Shoe
-#   attr_accessor :color, :size, :material, :condition
-#   attr_reader :brand
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
 
 # BRANDS = []
@@ -15,4 +15,4 @@
 #     puts "Your shoe is as good as new!"
 #   end
 
-# end
+end
